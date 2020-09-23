c0a1c6ec42f902690174c3ac79d98aea0879b384
xiaohuzi <604964872@qq.com>
2020-09-23 21:38:00 +0800

    删除内容

--- Test/src/main/java/com/Test1.java
+++ Test/src/main/java/com/Test1.java
@@ -5,11 +5,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello");
 
-        System.out.println("分支修改");
-        System.out.println("很nice");
 
-        System.out.println("主分支再次修改");
-
-        System.out.println("分支再次修改");
     }
 }
